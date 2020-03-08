d9a412fc26a6977e2b43dcfc191794994f300046
Alvaro Jair Martínez Varela <jtezva@gmail.com>
2020-03-08 14:24:36 -0600

    [jtezva] code cleaning

--- app/src/main/java/com/example/mywallet/account/dao/AccountContract.java
+++ app/src/main/java/com/example/mywallet/account/dao/AccountContract.java
@@ -1,18 +1,17 @@
 package com.example.mywallet.account.dao;
 
 public interface AccountContract {
-
-    public static final String SQL_CREATE_TACCOUNT = "CREATE TABLE " +
+    String SQL_CREATE_TACCOUNT = "CREATE TABLE " +
             AccountContract.TABLE_NAME + " ( " +
             AccountContract.COLUMN_NAME_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
             AccountContract.COLUMN_NAME_NAME + " TEXT, " +
             AccountContract.COLUMN_NAME_AMOUNT + " REAL "
             + ") ";
 
-    public static final String SQL_DELETE_TACCOUNT = "DROP TABLE IF EXISTS " + AccountContract.TABLE_NAME;
+    String SQL_DELETE_TACCOUNT = "DROP TABLE IF EXISTS " + AccountContract.TABLE_NAME;
 
-    public static final String TABLE_NAME = "TACCOUNT";
-    public static final String COLUMN_NAME_ID = "ID";
-    public static final String COLUMN_NAME_NAME = "NAME";
-    public static final String COLUMN_NAME_AMOUNT = "AMOUNT";
+    String TABLE_NAME = "TACCOUNT";
+    String COLUMN_NAME_ID = "ID";
+    String COLUMN_NAME_NAME = "NAME";
+    String COLUMN_NAME_AMOUNT = "AMOUNT";
 }
